ebc75706d279edfbe02070b1921faa501ccb7eea
USER <USER@DESKTOP-2OKNQSC>
2026-05-14 22:23:29 +0300

    qwqeqweqwe

--- StudentBinding/MainPage.xaml.cs
+++ StudentBinding/MainPage.xaml.cs
@@ -1,12 +1,64 @@
-﻿namespace StudentBinding
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using StudentBinding.Models;
+
+namespace StudentBinding
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
-        
+        Student student1;
+        Student student2;
+
+        private Student shownStudent;
+
+        public Student ShownStudent
+        {
+            get { return shownStudent; }
+            set
+            {
+                shownStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+
+            student1 = new Student
+            {
+                Name = "דניאל",
+                Gender = "זכר",
+                Image = "daniel.png",
+                BirthDate = new DateTime(2009, 4, 12)
+            };
+
+            student2 = new Student
+            {
+                Name = "נועה",
+                Gender = "נקבה",
+                Image = "noa.png",
+                BirthDate = new DateTime(2010, 9, 25)
+            };
+
+            ShownStudent = student1;
+
+            BindingContext = this;
         }
 
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        protected new void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (ShownStudent == student1)
+                ShownStudent = student2;
+            else
+                ShownStudent = student1;
+        }
     }
 }
---
+++ StudentBinding/Models/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace StudentBinding.Models
+{
+    public class Student : ObservableObject
+    {
+        private string name;
+        private int age;
+        private string image;
+        private DateTime birthDate;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Now.Year - BirthDate.Year;
+
+                if (DateTime.Now.DayOfYear < BirthDate.DayOfYear)
+                    age--;
+
+                return age;
+            }
+        }
+
+        public string Image
+        {
+            get { return image; }
+            set
+            {
+                image = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set
+            {
+                birthDate = value;
+                OnPropertyChanged();
+            }
+        }
+        private string gender;
+
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                gender = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
